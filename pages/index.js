3c81ee12c762b88af404e6b0c4e56ca50208d56d
Đặng Hoàng Long <tylbam11@gmail.com>
2022-11-29 14:21:02 +0700

    resolved navbar issue by downgrading nextjs, added more info to the main page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import { Container, Box, Heading, Image } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+    Container,
+    Box,
+    Heading,
+    Image,
+    Button,
+    Link,
+    useColorModeValue
+} from '@chakra-ui/react'
+import Section from '../components/section.js'
+import Paragraph from '../components/paragraph.js'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { BioSection, BioYear } from '../components/bio.js'
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" bg="gray" p={3} mb={6} mt={5} align="center">
-                Hi, I'm a sophomore from the University of
-                Science, VNU-HCM. My major is Information Technology.
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                p={3}
+                mb={6}
+                mt={5}
+                align="center"
+            >
+                Hi, I'm a sophomore from the University of Science, VNU-HCM. <br></br>
+                I major in Information Technology.
             </Box>
             <Box display={{ md: 'flex' }}>
                 <Box flexGrow={1}>
@@ -27,12 +47,58 @@ const Page = () => {
                         maxWidth="120px"
                         display="inline-block"
                         borderRadius="full"
-                    
                         src="/images/Lonk.jpg"
                         alt="Profile Image"
                     />
                 </Box>
             </Box>
+
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title" align="left">
+                    About me
+                </Heading>
+                <Paragraph>Content</Paragraph>
+                <Box align="center" my={4}>
+                    <NextLink href="https://www.linkedin.com/in/long-dang1109/">
+                        <Button
+                            colorScheme="teal"
+                            rightIcon={<ChevronRightIcon />}
+                        >
+                            My portfolio
+                        </Button>
+                    </NextLink>
+                </Box>
+            </Section>
+            <Section delay={0.2}>
+                <Heading as="h3" variant="section-title" align="left">
+                    Bio
+                </Heading>
+                <BioSection align="left">
+                    <BioYear>2003</BioYear>
+                    Born in Ho Chi Minh City, Vietnam
+                </BioSection>
+                <BioSection align="left">
+                    <BioYear>2018</BioYear>
+                    High School for the Gifted, VNU-HCM (English specialization)
+                </BioSection>
+                <BioSection align="left" padding-right="4em">
+                    <BioYear>2021 to present</BioYear>
+                    University of Science, VNU-HCM
+                </BioSection>
+
+                <Heading as="h3" variant="section-title" align="left">
+                    Hobbies
+                </Heading>
+                <Paragraph>Insert hobbies here</Paragraph>
+
+
+                <Heading as="h3" variant="section-title" align="left">
+                    Find me at
+                </Heading>
+                <Paragraph>
+                    Insert some social media info
+                </Paragraph>
+            </Section>
         </Container>
     )
 }

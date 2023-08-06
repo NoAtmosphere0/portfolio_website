6957608e9c49162e42786ae1b56a9413799e7c5e
Đặng Hoàng Long <tylbam11@gmail.com>
2023-08-06 12:00:48 +0700

    change href to 21127347

--- components/navbar.js
+++ components/navbar.js
@@ -105,7 +105,7 @@ const Navbar = props => {
                                 <NextLink href="/about" passHref>
                                     <MenuItem as={Link}>About</MenuItem>
                                 </NextLink>
-                                <NextLink href="../pages/21127347.html">
+                                <NextLink href="21127347">
                                     <MenuItem as={Link}>Works</MenuItem>
                                 </NextLink>
                                 <NextLink

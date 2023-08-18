e3d587a2d61b6f22cacfcf5e8173569b98e78ffc
Đặng Hoàng Long <tylbam11@gmail.com>
2023-08-18 12:09:35 +0700

    add loader in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     reactStrictMode: true,
+    future: {
+        webpack5: true,
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.module.rules.push({
+            test: /\.html$/i,
+            loader: "html-loader",
+        });
+        return config;
+    },
 
 }
 

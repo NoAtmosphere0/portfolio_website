c0171634e247e7f64e7d03588e5fcfe04fead07f
Đặng Hoàng Long <tylbam11@gmail.com>
2023-08-18 15:56:27 +0700

    made head space for the intro section of projects page

--- pages/projects.js
+++ pages/projects.js
@@ -5,6 +5,8 @@ export default function Illiteracy() {
     return (
         <div>
             <h1 id="projects">Projects</h1>
+            <h1></h1>
+            <h1></h1>
             <p>The code below is the code for analyzing the Illiteracy Rate of the Vietnam Population in 2020.</p>
             <p>This project looks the most interesting in my opinion so I will put it here.</p>
             <p>My other projects can be found <a href="https://github.com/NoAtmosphere0/projects"><strong>here</strong></a></p>

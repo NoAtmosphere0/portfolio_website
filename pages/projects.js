803bb6954b8ee79834939da90f256dce94024706
Đặng Hoàng Long <tylbam11@gmail.com>
2023-08-18 15:59:35 +0700

    more blank lines

--- pages/projects.js
+++ pages/projects.js
@@ -5,8 +5,11 @@ export default function Illiteracy() {
     return (
         <div>
             <h1 id="projects">Projects</h1>
-            <h1></h1>
-            <h1></h1>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
             <p>The code below is the code for analyzing the Illiteracy Rate of the Vietnam Population in 2020.</p>
             <p>This project looks the most interesting in my opinion so I will put it here.</p>
             <p>My other projects can be found <a href="https://github.com/NoAtmosphere0/projects"><strong>here</strong></a></p>

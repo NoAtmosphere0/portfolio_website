9f345fa4a952a94d1c907f2dfe906f4982471a46
Đặng Hoàng Long <tylbam11@gmail.com>
2023-08-18 11:08:23 +0700

    trying the .html thing

--- components/navbar.js
+++ components/navbar.js
@@ -71,7 +71,7 @@ const Navbar = props => {
                     flexGrow={1}
                     mt={{ base: 4, md: 0 }}
                 >
-                    <LinkItem href="/21127347" path={path}>
+                    <LinkItem href="/21127347.html" path={path}>
                         Projects
                     </LinkItem>
                     <LinkItem

813562dd2a901bfe215cf6be84c65b18065c147a
Đặng Hoàng Long <tylbam11@gmail.com>
2023-08-18 20:12:43 +0700

    moved Projects text down a lil bit

--- pages/projects.js
+++ pages/projects.js
@@ -4,10 +4,10 @@ import illiteracy from './illiteracy.html';
 export default function Illiteracy() {
     return (
         <div>
+            <br/>
+            <br/>
+            <br/>
             <h1 id="projects">Projects</h1>
-            <br/>
-            <br/>
-            <br/>
             <p>The code below is the code for analyzing the Illiteracy Rate of the Vietnam Population in 2020.</p>
             <p>This project looks the most interesting in my opinion so I will put it here.</p>
             <p>My other projects can be found here <b><a href="https://github.com/NoAtmosphere0/projects"><strong>(link)</strong></a></b></p>

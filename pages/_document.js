ad0988dd6ea9e05e0bc2ce4e559849e801438b2e
Đặng Hoàng Long <tylbam11@gmail.com>
2023-08-22 08:53:35 +0700

    add favicon via _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,9 @@ export default class Document extends NextDocument {
     render() {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+                <link rel="shortcut icon" href="/kirby.ico" />
+                </Head>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
                     <Main />
